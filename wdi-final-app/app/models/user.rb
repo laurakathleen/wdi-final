9341a82019e20694ff0781683dfcef6df73a596c
laurakathleen <laurakwalley@gmail.com>
2016-10-28 19:16:53 -0700

    sets up users routes to login/sign up form

---
+++ wdi-final-app/app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+	has_secure_password
+
+	def self.confirm(params)
+		@user = User.find_by({username: params[:username]})
+		@user ? @user.authenticate(params[:password]) : false
+	end
+end

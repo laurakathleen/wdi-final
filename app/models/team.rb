4ca5a2a8f9e5d56cbdecf6679c24ed9183a429e7
laurakathleen <laurakwalley@gmail.com>
2016-11-27 16:35:10 -0800

    filters teams by uniqueness

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,4 +2,6 @@ class Team < ApplicationRecord
 	has_many :team_users, dependent: :destroy
 	has_many :users, through: :team_users
 	has_many :logs, through: :users
+	validates :users, uniqueness: true
+	validates_uniqueness_of :name
 end
--- app/models/team_user.rb
+++ app/models/team_user.rb
@@ -2,5 +2,5 @@ class TeamUser < ApplicationRecord
   belongs_to :user
   belongs_to :team
 
-  validates_uniqueness_of :user_id, :scope => :team_id
+  validates_uniqueness_of :team_id, :scope => :user_id
 end
